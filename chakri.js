2c3aa298157160f80409deb783a52d71feb0b982
SaiChakri-d <98957092+SaiChakri-d@users.noreply.github.com>
2022-02-16 14:59:35 +0530

    Add files via upload

---
+++ chakri.js
@@ -0,0 +1,16 @@
+1. Find the greatest of 2 numbers.
+
+var a = 10;
+var b = 10;
+if(a>b)
+{
+   console.log('A is greater')
+}
+else if(b>a)
+{
+   console.log('B is greater')
+}
+else
+{
+   console.log('Both are equall')
+}
